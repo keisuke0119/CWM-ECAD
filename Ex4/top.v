93079978ca59809d69545fb87b030afd3c57a701
Cloud User <centos@ip-172-31-13-184.ap-northeast-2.compute.internal>
2020-06-16 13:34:33 +0000

    working progress

--- Ex4/top.v
+++ Ex4/top.v
@@ -16,3 +16,21 @@
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns/100ps
+
+module dice(clk,rst,button,throw);
+
+	input clk,rst,button;
+	output reg [2:0]throw;
+
+	always @(posedge clk)
+	begin
+	if (rst || throw ==3'b111)
+		throw <= 3'b001;
+	else if (button && throw !=3'b110)
+		throw <= throw +1;
+	else if (button && throw == 3'b110)
+		throw <=3'b001;
+	end
+
+endmodule
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -7,3 +7,26 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns/100ps
+
+module top_tb();
+
+parameter CLK_PERIOD = 10;
+
+reg clk, rat, button;
+wire [2:0]throw;
+
+initial begin  
+rst = 1;
+#10
+rst = 0;
+button = 1;
+#100
+button = 0;
+$display("throw=%h",throw);
+end
+
+dice my_dice(clk,rst,button,throw);
+
+endmodule
+
